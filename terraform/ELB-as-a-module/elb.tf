444eada7730ebe687db7d30ebd0b9312fecdcdd2
lidor ettinger <lidor.ettinger@gmail.com>
2021-12-14 20:20:49 +0200

    add elb

--- terraform/ELB-as-a-module/elb.tf
+++ terraform/ELB-as-a-module/elb.tf
@@ -25,7 +25,7 @@ module "elb_http" {
       instance_protocol = "http"
       lb_port           = 8080
       lb_protocol       = "http"
-      ssl_certificate_id = "arn:aws:acm:eu-west-1:235367859451:certificate/6c270328-2cd5-4b2d-8dfd-ae8d0004ad31"
+      ssl_certificate_id = ""
     },
   ]
 
--- terraform/ELB-as-a-module/variables.tf
+++ terraform/ELB-as-a-module/variables.tf
@@ -1,11 +1,3 @@
-variable "cluster_name" {
-  default = "echo-server-app"
-}
-
-variable "eks_version" {
-  default = "1.18"
-}
-
 variable "instance_type" {
   description = "Instance type for the node group"
   default     = "t2.medium"
@@ -13,21 +5,4 @@ variable "instance_type" {
 
 variable "region" {
   default = "eu-west-2"
-}
-
-variable "vpc_cidr" {
-  default = "10.1.0.0/16"
-}
-
-variable "app_name" {
-  default = "echo-server"
-}
-
-variable "app_version" {
-  default = "v1.0.0"
-}
-
-variable "env_name" {
-  description = "Will be used as tags for the node_groups and namespace on k8s"
-  default     = "playground"
 }
