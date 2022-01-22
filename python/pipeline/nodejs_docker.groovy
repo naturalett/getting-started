88b47c72989e48867c14ba2837d783fbd21a2fc9
lidor ettinger <lidor.ettinger@gmail.com>
2022-01-22 19:11:57 +0200

    add nodejs docker pipeline

---
+++ python/pipeline/nodejs_docker.groovy
@@ -0,0 +1,25 @@
+job('NodeJS Docker example') {
+    scm {
+        git('git://github.com/yanivomc/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@devophift.work')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+
+
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('naturalett/playground')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('lidor-dockerhub')
+            buildContext('./basics/')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
