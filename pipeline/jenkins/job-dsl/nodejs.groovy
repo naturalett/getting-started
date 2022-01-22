3d6649c6ef4db4270530db3a678fada1632f4a48
lidor ettinger <lidor.ettinger@gmail.com>
2022-01-22 19:15:33 +0200

    add nodejs docker pipeline

---
+++ pipeline/jenkins/job-dsl/nodejs.groovy
@@ -0,0 +1,45 @@
+job('NodeJS Moshe') {
+    scm {
+        git('git://github.com/yanivomc/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('1 * * * *')
+    }
+    wrappers {
+        nodejs('NodeJs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install agaim")
+    }
+}
+
+job('NodeJS Docker') {
+    scm {
+        git('git://github.com/yanivomc/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') 
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('yanivomc/docker-nodejs-demo') //qa / dev
+            buildContext('./basics')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
--- pipeline/jenkins/job-dsl/nodejs_docker.groovy
+++ pipeline/jenkins/job-dsl/nodejs_docker.groovy
@@ -1,21 +1,21 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/yanivomc/docker-cicd.git','master') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@devophift.work')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
-
-
+    wrappers {
+        nodejs('nodejs-new') 
+    }
     steps {
         dockerBuildAndPublish {
-            repositoryName('naturalett/playground')
+            repositoryName('aaaaaaaaa/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('lidor-dockerhub')
-            buildContext('./basics/')
+            registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
